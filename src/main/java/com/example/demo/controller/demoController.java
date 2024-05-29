e8e0c7369f7331e606b6f99afa9477d430fe2eb5
singhsatya458 <singhsaty6@gmail.com>
2024-05-30 01:25:59 +0530

    Update demoController.java

--- src/main/java/com/example/demo/controller/demoController.java
+++ src/main/java/com/example/demo/controller/demoController.java
@@ -5,5 +5,6 @@ public class demoController {
         System.out.println("Hello world");
         System.out.println("Hello world");
         System.out.println("Hello world");
+        System.out.println("Hello world");
     }
 }
