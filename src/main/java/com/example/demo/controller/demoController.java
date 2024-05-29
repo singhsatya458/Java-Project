80cd33c0b3e6d3f79d2105e4fa4ee998f1d3d8e6
singhsatya458 <singhsaty6@gmail.com>
2024-05-30 01:08:08 +0530

    added hellow world in controller

--- src/main/java/com/example/demo/controller/demoController.java
+++ src/main/java/com/example/demo/controller/demoController.java
@@ -3,5 +3,6 @@ package com.example.demo.controller;
 public class demoController {
     public void test(){
         System.out.println("Hello world");
+        System.out.println("Hello world");
     }
 }
